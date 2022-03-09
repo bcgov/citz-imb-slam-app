07d4f8905cf579267d526d8271ac6a19b087b0db
aideninbcgov <aiden.in@gov.bc.ca>
2022-03-09 09:44:23 -0800

    Sync

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,11 @@
 /** @format */
-import { Navigation, SoftwareTable } from 'components';
+import { SoftwareTable } from 'components';
 /**
  * the home page
  * @returns {React.jsx}
  */
 export default function Home() {
 	return (
-		<>
-			<Navigation />
 			<SoftwareTable />
-		</>
 	);
 }
