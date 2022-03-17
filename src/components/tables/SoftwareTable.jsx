b3a35a38f24c742449aff63cbf2366e1a5d75001
Scott Toews <scott.toews@gov.bc.ca>
2022-03-17 14:02:35 -0700

    added quantity assigned

--- src/components/tables/SoftwareTable.jsx
+++ src/components/tables/SoftwareTable.jsx
@@ -4,12 +4,18 @@ import { TableHeader } from './common/TableHeader';
 import { useSoftware } from 'hooks';
 import { useRouter } from 'next/router';
 import clsx from 'clsx';
+import { QuantityAssigned } from './common/QuantityAssigned';
+import { Button } from 'components';
+import { UserSearchDialog } from 'components/forms/UserSearchDialog';
+import { IconButton, Stack } from '@mui/material';
+import MoreIcon from '@mui/icons-material/More';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+
 /**
  * Presents a table with all the Software Titles listed.
  * @returns {React.jsx}
  */
 export const SoftwareTable = () => {
-
 	const columns = [
 		{ field: 'id', headerName: 'ID', width: 300 },
 		{ field: 'title', headerName: 'Software Title', width: 250 },
@@ -19,21 +25,50 @@ export const SoftwareTable = () => {
 			headerName: 'Renewal Date',
 			type: 'date',
 			width: 200,
-			valueFormatter: (params) => new Date(params.value).toISOString().split('T')[0].replace(/ /g, '-'),
+			valueFormatter: (params) =>
+				new Date(params.value).toISOString().split('T')[0].replace(/ /g, '-'),
 			cellClassName: (params) =>
-			clsx('cell-render', {
-			  warning: new Date(params.value) < new Date().setDate(new Date().getDate() + 30),
-			}),
+				clsx('cell-render', {
+					warning:
+						new Date(params.value) <
+						new Date().setDate(new Date().getDate() + 30),
+				}),
 		},
 		{
 			field: 'quantity',
 			headerName: 'Quantity',
 			width: 150,
+			renderCell: (params) => {
+				return (
+					<QuantityAssigned
+						assigned={params.row.__licenseeConnection__.length}
+						available={params.row.quantity}
+					/>
+				);
+			},
 		},
 		{
 			field: 'administrator',
 			headerName: 'Licence Administrator',
 			width: 200,
+		},
+		{
+			field: 'actions',
+			headerName: 'Actions',
+			width: 150,
+			renderCell: (params) => {
+				return (
+					<Stack direction={'row'} spacing={0}>
+						<IconButton
+							color='primary'
+							aria-label='go to details'
+							onClick={onDetailsClick}>
+							<MoreIcon />
+						</IconButton>
+						<UserSearchDialog id={params.row.id} />
+					</Stack>
+				);
+			},
 		},
 	];
 
@@ -48,7 +83,7 @@ export const SoftwareTable = () => {
 
 	const router = useRouter();
 
-	const onRowClick = useCallback(
+	const onDetailsClick = useCallback(
 		(props) => {
 			router.push(`/software/${props.id}`);
 		},
@@ -67,7 +102,6 @@ export const SoftwareTable = () => {
 					listName={'software'}
 					columns={columns}
 					rows={rows}
-					onRowClick={onRowClick}
 					isError={isError}
 					isLoading={isLoading}
 				/>
