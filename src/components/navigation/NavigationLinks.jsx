93db33e693dd8b6bb3c7c6ff72fb2d573a9292c7
aideninbcgov <70336793+aideninbcgov@users.noreply.github.com>
2022-05-04 15:55:38 -0700

    fix

--- src/components/navigation/NavigationLinks.jsx
+++ src/components/navigation/NavigationLinks.jsx
@@ -9,34 +9,6 @@ export const NavigationLinks = () => {
   const router = useRouter();
   const { status } = useSession();
 
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#5B7CFD",
-        text: "#222222",
-      },
-      secondary: {
-        main: "#fff",
-        bg: "#fff",
-      },
-    },
-    typography: {
-      fontFamily: [
-        "-apple-system",
-        "BlinkMacSystemFont",
-        '"Segoe UI"',
-        "Roboto",
-        '"Helvetica Neue"',
-        "Arial",
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(","),
-      fontWeight: 400,
-    },
-  });
-
   return (
     <ThemeProvider theme={Theme}>
       <Stack direction="row" spacing={2}>
