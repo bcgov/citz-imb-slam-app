6272dd3390dcc972148433218995ef83632ac44b
Scott Toews <scott.toews@gov.bc.ca>
2022-05-05 12:04:24 -0700

    added console.log to troubleshoot

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,11 @@
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
+console.log('github', {
+  id: process.env.GITHUB_ID,
+  secret: process.env.GITHUB_SECRET,
+});
+
 export default NextAuth({
   providers: [
     GitHubProvider({
