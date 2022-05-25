186e996fff79b685a0cd16ea34caf0bb4bf843cd
Scott Toews <scott.toews@gov.bc.ca>
2022-05-25 07:13:45 -0700

    initial jwt fetch

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,37 +1,51 @@
 /** @format */
-import { Card, Grid, Stack, Typography } from '@mui/material';
+import { Button, Card, Grid, Stack, Typography } from '@mui/material';
 import { Unauthorized } from 'components';
+import { useAPI } from 'hooks';
 import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 /**
  * the home page
  * @returns {React.jsx}
  */
 export default function Home() {
-	const boxSX = { width: 300, height: 300, border: '1px solid grey' };
+  const boxSX = { width: 300, height: 300, border: '1px solid grey' };
 
-	const { status } = useSession();
+  const { data: session, status } = useSession();
 
-	return (
-		<Grid container>
-			<Stack direction='column' spacing={3}>
-				<Typography variant='h4'>Welcome to SLAM</Typography>
-				{status !== 'authenticated' && <Unauthorized />}
-				<div>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta quae
-					dignissimos minima recusandae sit? Veniam minus nihil praesentium
-					error. Provident, eligendi assumenda ab sit pariatur quasi inventore
-					quos tenetur a.
-				</div>
-				<Stack direction='row' spacing={2}>
-					<Card sx={boxSX}>box</Card>
-					<Card sx={boxSX}>box</Card>
-					<Card sx={boxSX}>box</Card>
-				</Stack>
-				<Stack direction='row'>
-					<Typography variant='h5'>lorem ipsum</Typography>
-				</Stack>
-				<div>footer</div>
-			</Stack>
-		</Grid>
-	);
+  const { login } = useAPI();
+
+  useEffect(() => {
+    console.log('status', status);
+    if (status === 'authenticated') {
+      login(session.user.name);
+    }
+
+    return () => {};
+  }, [login, status, session]);
+
+  return (
+    <Grid container>
+      <Stack direction="column" spacing={3}>
+        <Typography variant="h4">Welcome to SLAM</Typography>
+        {status !== 'authenticated' && <Unauthorized />}
+        <Button onClick={login}>Click me</Button>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta quae
+          dignissimos minima recusandae sit? Veniam minus nihil praesentium
+          error. Provident, eligendi assumenda ab sit pariatur quasi inventore
+          quos tenetur a.
+        </div>
+        <Stack direction="row" spacing={2}>
+          <Card sx={boxSX}>box</Card>
+          <Card sx={boxSX}>box</Card>
+          <Card sx={boxSX}>box</Card>
+        </Stack>
+        <Stack direction="row">
+          <Typography variant="h5">lorem ipsum</Typography>
+        </Stack>
+        <div>footer</div>
+      </Stack>
+    </Grid>
+  );
 }
