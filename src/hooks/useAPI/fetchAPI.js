6486c52ba649b82ed2ef2eb7b6c00d6f6abb9b76
Scott Toews <scott.toews@gov.bc.ca>
2022-06-09 10:03:08 -0700

    fix more typos

--- src/hooks/useAPI/fetchAPI.js
+++ src/hooks/useAPI/fetchAPI.js
@@ -3,7 +3,7 @@ export const fetchAPI = async (endPoint, options) => {
   const API_PORT = process.env.NEXT_PUBLIC_API_PORT || ':80'
   const API_PATH = process.env.NEXT_PUBLIC_API_PATH || 'api/v1'
 
-  const url = `${window.location.protocol}//${window.location.hostname}${$NEXT_PUBLIC_API_PORT}/${$API_PATH}/${endPoint}`;
+  const url = `${window.location.protocol}//${window.location.hostname}${API_PORT}/${API_PATH}/${endPoint}`;
   console.log('url>>', url)
   const response = await fetch(url, options);
 
