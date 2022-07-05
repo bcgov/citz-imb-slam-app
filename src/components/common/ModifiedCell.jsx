afb0388c038e8c902326f0713a07250fd2761202
Matthew Sielecki <msielecki2@gmail.com>
2022-07-05 09:30:57 -0700

    fixed formatting

--- src/components/common/ModifiedCell.jsx
+++ src/components/common/ModifiedCell.jsx
@@ -1,5 +1,5 @@
 import { Typography } from '@mui/material';
-import { DateTime, utcInstance } from 'luxon';
+import { DateTime } from 'luxon';
 import { useEffect, useState } from 'react';
 
 const MINUTES = 60;
@@ -8,14 +8,12 @@ const SECONDS = 60;
 const DAYS = 28;
 
 export const ModifiedCell = ({ value }) => {
-
   // Date is changed from UTC to PST then formated to ISO
+  const modifiedDate = value.row.modified.replace('Z', '+07:00');
+  const date = DateTime.fromISO(modifiedDate);
 
-  const modifiedDate = (value.row.modified).replace('Z', "+07:00");
-  const date = (DateTime.fromISO(modifiedDate));
-
-  function timeDifference(time, interval){
-    return Math.abs((time.diff(DateTime.now())).as(interval));
+  function timeDifference(time, interval) {
+    return Math.abs(time.diff(DateTime.now()).as(interval));
   }
 
   const [modified, setModified] = useState({
@@ -26,20 +24,25 @@ export const ModifiedCell = ({ value }) => {
 
   useEffect(() => {
     if (timeDifference(date, 'seconds') < SECONDS) {
-      setModified({ text: Math.round(timeDifference(date, 'seconds')) + ' seconds ago' })
-
+      setModified({
+        text: `${Math.round(timeDifference(date, 'seconds'))} seconds ago`,
+      });
     } else if (timeDifference(date, 'minutes') < MINUTES) {
-      setModified({ text: Math.round(timeDifference(date, 'minutes')) + ' minutes ago' })
-
+      setModified({
+        text: `${Math.round(timeDifference(date, 'minutes'))} minutes ago`,
+      });
     } else if (timeDifference(date, 'hours') < HOURS) {
-      setModified({ text: Math.round(timeDifference(date, 'hours')) + ' hours ago' })
-
+      setModified({
+        text: `${Math.round(timeDifference(date, 'hours'))} hours ago`,
+      });
     } else if (timeDifference(date, 'days') < DAYS) {
-      setModified({ text: Math.round(timeDifference(date, 'days')) + ' days ago' })
-
+      setModified({
+        text: `${Math.round(timeDifference(date, 'days'))} days ago`,
+      });
     } else {
-      setModified({text:date.setLocale('en-ca').toLocaleString(DateTime.DATE_MED)})
-
+      setModified({
+        text: date.setLocale('en-ca').toLocaleString(DateTime.DATE_MED),
+      });
     }
     return () => {};
   }, []);
@@ -51,10 +54,9 @@ export const ModifiedCell = ({ value }) => {
       sx={{
         backgroundColor: 'none',
         color: { ...modified },
-        fontWeight: { ...modified},
+        fontWeight: { ...modified },
       }}
       variant="span"
-      // icon={false}
     >
       {modified.text}
     </Typography>
