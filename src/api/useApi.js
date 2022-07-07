2f69b600bc3f5b0a5887c76ec0772d6b861bf4f7
Scott Toews <scott.toews@gov.bc.ca>
2022-07-07 13:23:12 -0700

    basic api interaction

---
+++ src/api/useApi.js
@@ -0,0 +1,24 @@
+export const useApi = () => {
+  const PORT = process.env.REACT_APP_PORT || ''
+
+  const BASEURL = `${window.location.protocol}//${window.location.hostname}${PORT}/api/v1`
+  const BASEOPTIONS = {
+    method: 'GET',
+    headers: {
+      Accept: '*/*',
+      'Access-Control-Request-Method': 'GET',
+      'Content-Type': 'application/json'
+    },
+  }
+
+  const getRoot = async () => {
+    const response = await fetch(`${BASEURL}`, BASEOPTIONS)
+
+    if (response.ok) {
+      const payload = await response.json()
+      return payload
+    }
+  }
+
+  return { getRoot }
+}
