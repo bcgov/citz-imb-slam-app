c0a0c48a2fd228165be70966362f754458f7e8af
Scott Toews <scott.toews@gov.bc.ca>
2022-07-08 12:16:13 -0700

    auth

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import { useApi } from './api/useApi'
 function App() {
 
   const [value, setValue] = useState('initial state')
+  const [authText, setAuthText] = useState('login')
 
-  const { getRoot } = useApi()
+  const { getRoot, login } = useApi()
 
   const clickHandler = async () => {
     const response = await getRoot()
-
+    console.log('response', response)
     setValue(response.message)
+  }
+  const authClickHandler = async () => {
+    const response = await login()
+    console.log('response', response)
+    setAuthText('logged in')
   }
 
   return (
     <div className="App">
       <button onClick={clickHandler}>Click Me</button>
       <div>{value}</div>
+      <button onClick={authClickHandler}>{authText}</button>
     </div>
   );
 }
--- src/api/useApi.js
+++ src/api/useApi.js
@@ -4,21 +4,48 @@ export const useApi = () => {
   const BASEURL = `${window.location.protocol}//${window.location.hostname}${PORT}/api/v1`
   const BASEOPTIONS = {
     method: 'GET',
+    credentials: 'include',
     headers: {
       Accept: '*/*',
       'Access-Control-Request-Method': 'GET',
       'Content-Type': 'application/json'
     },
   }
-  console.log('BASEURL', BASEURL)
+
   const getRoot = async () => {
     const response = await fetch(`${BASEURL}`, BASEOPTIONS)
 
     if (response.ok) {
       const payload = await response.json()
+      console.log('payload', payload)
       return payload
     }
   }
 
-  return { getRoot }
+  const login = async () => {
+
+    const headers = {
+      ...BASEOPTIONS.headers,
+      'Access-Control-Request-Method': 'POST',
+    }
+
+    const options = {
+      ...BASEOPTIONS,
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        username: 'Scott',
+        password: 'pass'
+      })
+    }
+    const response = await fetch(`${BASEURL}/auth/login`, options)
+
+    if (response.ok) {
+      // const payload = await response.json()
+      // console.log('payload', payload)
+      // return payload
+    }
+  }
+
+  return { getRoot, login }
 }
