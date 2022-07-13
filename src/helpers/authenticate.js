bbb323d3373f5b7d7401ebad1e475bf05a2de4ed
Scott Toews <scott.toews@gov.bc.ca>
2022-07-13 12:25:58 -0700

    removed session required

--- src/helpers/authenticate.js
+++ src/helpers/authenticate.js
@@ -2,15 +2,15 @@ import { getSession } from 'next-auth/react';
 
 export const authenticate = async (context) => {
   const session = await getSession(context);
-  console.log('session', { context, session })
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/api/auth/signin',
-        permanent: false,
-      },
-    };
-  }
+  console.log('session', { context, session });
+  // if (!session) {
+  //   return {
+  //     redirect: {
+  //       destination: '/api/auth/signin',
+  //       permanent: false,
+  //     },
+  //   };
+  // }
 
   return { props: {} };
 };
