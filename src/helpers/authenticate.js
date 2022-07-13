8206a0764f858fca895016251410fd64dda3e43b
Scott Toews <scott.toews@gov.bc.ca>
2022-07-13 12:18:40 -0700

    added console.log

--- src/helpers/authenticate.js
+++ src/helpers/authenticate.js
@@ -2,7 +2,7 @@ import { getSession } from 'next-auth/react';
 
 export const authenticate = async (context) => {
   const session = await getSession(context);
-
+  console.log('session', { context, session })
   if (!session) {
     return {
       redirect: {
