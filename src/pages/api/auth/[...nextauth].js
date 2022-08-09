af0882b540e276274bc775fcbb7144c0bad0c22b
Matthew Sielecki <msielecki2@gmail.com>
2022-08-09 15:58:41 -0700

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,25 +4,10 @@ import KeycloakProvider from 'next-auth/providers/keycloak';
 export default NextAuth({
   providers: [
     KeycloakProvider({
-      issuer: 'https://dev.loginproxy.gov.bc.ca/auth/realms/standard',
-      clientId: 'slam-3943',
-      clientSecret: 'some secret',
+      issuer: process.env.NEXT_PUBLIC_ISSUER,
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
     }),
   ],
 
-  // callbacks: {
-  //   async redirect({ url, baseUrl }) {
-  //     // Allows relative callback URLs
-  //     if (url.startsWith('/')) return `${baseUrl}${url}`;
-  //     // Allows callback URLs on the same origin
-  //     if (new URL(url).origin === baseUrl) return url;
-  //     return baseUrl;
-  //   },
-
-  //   async session({ session, token, user }) {
-  //     // Send properties to the client, like an access_token from a provider.
-  //     session.accessToken = token.accessToken;
-  //     return session;
-  //   },
-  // },
 });
